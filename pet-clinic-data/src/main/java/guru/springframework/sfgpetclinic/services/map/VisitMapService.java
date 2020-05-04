1450b1a9fb86b3a2427ebff5d9fd9e7f06129364
lakemaster <lakelord@jojobi.de>
2020-05-04 09:06:09 +0200

    Add visit map service. Closes #46.

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/VisitMapService.java
@@ -0,0 +1,15 @@
+package guru.springframework.sfgpetclinic.services.map;
+
+import guru.springframework.sfgpetclinic.model.Visit;
+import guru.springframework.sfgpetclinic.services.VisitService;
+
+public class VisitMapService extends AbstractMapService<Visit, Long> implements VisitService {
+    @Override
+    public Visit save(Visit visit) {
+        if ( visit.getPet() == null || visit.getPet().getId() == 0
+        || visit.getPet().getOwner() == null || visit.getPet().getOwner().getId() == 0 ) {
+            throw new RuntimeException("Invalid Visit");
+        }
+        return super.save(visit);
+    }
+}
