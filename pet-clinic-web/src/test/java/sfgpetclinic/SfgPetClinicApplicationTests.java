4308f0844d8c6e1f873884b609f60c015dbc5068
lakemaster <lakelord@jojobi.de>
2020-05-05 13:16:41 +0200

    Add Junit5 - surefire-plugin. Closes #4.

--- pet-clinic-web/src/test/java/sfgpetclinic/SfgPetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -2,8 +2,11 @@ package sfgpetclinic;
 
 import guru.springframework.sfgpetclinic.MyComponent;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+@ExtendWith(SpringExtension.class)
 @SpringBootTest(classes = MyComponent.class)
 class SfgPetClinicApplicationTests {
 
